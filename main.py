f29e303c4bbe437ab5829ea0f61f6298c3347adb
k8tems <k8tems@live.com>
2018-07-03 23:51:48 +0900

    Add method to parse input

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import json
 
 
+def parse(text):
+    pass
+
+
 def main():
     root_node = {
         'root': {'id': "hyfkdnca",
                  'text': 'My Mind Map',
                  'layout': 'map',
                  'children': []}}
+    with open('test.txt') as f:
+        parse(f.read())
     with open('out.mymind', 'w') as f:
         f.write(json.dumps(root_node))
 
