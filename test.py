81e96d17f444adac1148f6fe8717c868cdc8042f
k8tems <k8tems@live.com>
2018-07-04 00:07:54 +0900

    Add failing test for `convert`

--- test.py
+++ test.py
@@ -3,7 +3,20 @@ import main
 
 
 class TestCase(unittest.TestCase):
-    def test(self):
+    def test_convert(self):
+        input = {'a': ['b'], 'c': ['d']}
+        expected = [
+            {'children': [{'children': [], 'id': None, 'side': 'left', 'text': 'b'}],
+             'id': None,
+             'side': 'left',
+             'text': 'a'},
+            {'children': [{'children': [], 'id': None, 'side': 'left', 'text': 'd'}],
+             'id': None,
+             'side': 'left',
+             'text': 'c'}]
+        self.assertEqual(expected, main.convert(input))
+
+    def test_parse(self):
         input = 'a\n    b\nc\n    d'
         expected = {'a': ['b'], 'c': ['d']}
         self.assertEqual(expected, main.parse(input))
