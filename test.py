e6dcd37af33f237966d33e7d4d4a407bc6fcb66f
k8tems <k8tems@live.com>
2018-07-04 23:47:57 +0900

    Adjust fixture

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ import main
 class TestCase(unittest.TestCase):
     def test_parse(self):
         input = [
-            ('a', 0), ('b', 1),
-            ('c', 0), ('d', 1)]
+            ('a', (0, 0)), ('b', (1, 0)),
+            ('c', (0, 1)), ('d', (1, 1))]
         expected = [
             {'children': [{'children': [], 'id': None, 'side': 'left', 'text': 'b'}],
              'id': None,
